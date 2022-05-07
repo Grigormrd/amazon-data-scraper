843d2516e17041ded3338906f339bb9206d29eb1
Grigor Muradyan <grigormrd@icloud.com>
2022-05-07 18:56:38 +0200

    delete urls file

--- urls.js
+++
@@ -1,4 +0,0 @@
-`${baseUrl}&url=https://www.amazon.com/dp/${productId}`;
-`${baseUrl}&url=https://www.amazon.com/product-reviews/${productId}`;
-`${baseUrl}&url=https://www.amazon.com/gp/offer-listing/${productId}`;
-`${baseUrl}&url=https://www.amazon.com/s?k=${searchQuery}`;
